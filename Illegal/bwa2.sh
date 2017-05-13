af0c6abb04cfcfb8bc2a23f43d12a06a0a103f43
UG-1vFiayb <holtuksss@gmail.com>
2017-05-13 02:22:09 -0500

    Update bwa2.sh

--- Illegal/bwa2.sh
+++ Illegal/bwa2.sh
@@ -20,49 +20,49 @@ sleep 2;
 echo -e "\x1B[01;95m $This is your last chance to quit this program$ \x1B[0;94m"
 sleep 1;
 echo -e "\x1B[96m $Press \e[0;34m ENTER \x1B[96m to continue, or \e[0;34m ESCAPE \x1B[96m to quit$ \x1B[0m"
-sleep 1;
+sleep 4;
 echo -e "\e[91m WELCOME TO HELL BROUGHT TO YOU BY KINGOREN - FUCK THE WORLD \e[0m"
 sleep 1;
 echo -e "\e[91m YOU'RE ALL TO BLAME, I HOPE YOU'RE HAPPY YOU CAUSED THIS \e[0m"
 sleep 1;
 echo -e "\e[91m  \e[0m"
 sleep 2:
-echo -e "\e[94m $                                            .""--..__
-                     _                     []       ``-.._
-                  .'` `'.                  ||__           `-._
-                 /    ,-.\                 ||_ ```---..__     `-.
-                /    /:::\\               /|//}          ``--._  `.
-                |    |:::||              |////}                `-. \
-                |    |:::||             //'///                    `.\
-                |    |:::||            //  ||'                      `|
-        666     /    |:::|/        _,-//\  ||
-        KO     /`    |:::|`-,__,-'`  |/  \ ||
-             /`  |   |'' ||           \   |||
-           /`    \   |   ||            |  /||
-         |`       |  |   |)            \ | ||
-        |          \ |   /      ,.__    \| ||
-        /           `         /`    `\   | ||
-       |                     /        \  / ||
-       |                     |        | /  ||
-       /         /           |        `(   ||
-      /          .           /          )  ||
-     |            \          |     ________||
-    /             |          /     `-------.|
-   |\            /          |              ||
-   \/`-._       |           /              ||
-    //   `.    /`           |              ||
-   //`.    `. |             \              ||
-  ///\ `-._  )/             |              ||
- //// )   .(/               |              ||
- ||||   ,'` )               /              //
- ||||  /                    /             || 
- `\\` /`                    |             // 
-     |`                     \            ||  
-    /                        |           //  
-  /`                          \         //   
-/`                            |        ||    
-`-.___,-.      .-.        ___,'        (/    
-         `---'`   `'----'` \e[0m"
+echo -e "\e[94m $                                            .""--..__ \e[0m"
+echo -e "\e[94m $                     _                     []       ``-.._ \e[0m"
+echo -e "\e[94m $                  .'` `'.                  ||__           `-._ \e[0m"
+echo -e "\e[94m $                 /    ,-.\                 ||_ ```---..__     `-. \e[0m"
+echo -e "\e[94m $                /    /:::\\               /|//}          ``--._  `. \e[0m"
+echo -e "\e[94m $                |    |:::||              |////}                `-. \ \e[0m" 
+echo -e "\e[94m $                |    |:::||             //'///                    `.\ \e[0m"
+echo -e "\e[94m $                |    |:::||            //  ||'                      `| \e[0m"
+echo -e "\e[94m $        666     /    |:::|/        _,-//\  || \e[0m"
+echo -e "\e[94m $        KO     /`    |:::|`-,__,-'`  |/  \ || \e[0m"
+echo -e "\e[94m $             /`  |   |'' ||           \   ||| \e[0m"
+echo -e "\e[94m $           /`    \   |   ||            |  /|| \e[0m"
+echo -e "\e[94m $         |`       |  |   |)            \ | || \e[0m"
+echo -e "\e[94m $        |          \ |   /      ,.__    \| || \e[0m"
+echo -e "\e[94m $        /           `         /`    `\   | || \e[0m"
+echo -e "\e[94m $       |                     /        \  / || \e[0m"
+echo -e "\e[94m $       |                     |        | /  || \e[0m"
+echo -e "\e[94m $       /         /           |        `(   || \e[0m"
+echo -e "\e[94m $      /          .           /          )  || \e[0m"
+echo -e "\e[94m $     |            \          |     ________|| \e[0m"
+echo -e "\e[94m $    /             |          /     `-------.| \e[0m"
+echo -e "\e[94m $   |\            /          |              || \e[0m"
+echo -e "\e[94m $   \/`-._       |           /              || \e[0m"
+echo -e "\e[94m $    //   `.    /`           |              || \e[0m"
+echo -e "\e[94m $   //`.    `. |             \              || \e[0m"
+echo -e "\e[94m $  ///\ `-._  )/             |              || \e[0m"
+echo -e "\e[94m $ //// )   .(/               |              || \e[0m"
+echo -e "\e[94m $ ||||   ,'` )               /              // \e[0m"
+echo -e "\e[94m $ ||||  /                    /             || \e[0m"
+echo -e "\e[94m $ `\\` /`                    |             // \e[0m"
+echo -e "\e[94m $     |`                     \            ||  \e[0m"
+echo -e "\e[94m $    /                        |           //  \e[0m"
+echo -e "\e[94m $  /`                          \         //   \e[0m"
+echo -e "\e[94m $/`                            |        ||   \e[0m" 
+echo -e "\e[94m $`-.___,-.      .-.        ___,'        (/    \e[0m"
+echo -e "\e[94m $         `---'`   `'----'` \e[0m"
 sleep 5;
 echo -e "\e[96 usage: >> bash theend.sh site retrieval:offline:scan KingOren \x1B[0m"
 
