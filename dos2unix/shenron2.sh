8fb68dfa424a1c4cdfacee02955dc246c524354c
UG-1vFiayb <holtuksss@gmail.com>
2017-05-14 08:32:46 -0500

    Create shenron2.sh

---
+++ dos2unix/shenron2.sh
@@ -0,0 +1,49 @@
+#! /bin/bash
+echo -e "\e[96m ## Welcome To SHENRON, Programmed By KingOren ## "
+sleep 2;
+echo -e "\e[96m ## Do NOT use this program if you don't know ASM ## "
+sleep 1;
+echo -e "\e[96m ## YOU WILL BRICK YOUR FUCKING BOX AND I AM NOT LIABLE ## "
+echo -e "\e[96m ## IF YOU STILL WANT TO USE THIS PROGRAM, PRESS ENTER ## "
+sleep 8;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep .5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ "
+sleep .5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep .5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ "
+sleep .5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep .5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ "
+sleep .5; 
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep .5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ "
+sleep .5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep .5;
+echo -e "\e[91m ___|___|___|___|___|___|___|___|___|__ "
+sleep .5;
+echo -e "\e[91m _|___|___|___|___|___|___|___|___|___| "
+sleep 3;
+clear
+echo -e "\e[91m        WELCOME TO \e[91mHELL "
+sleep 2;
+echo -e "\e[34                ,____                           ____, "
+echo -e "\e[34                |---.\                         /.---| "
+echo -e "\e[34                |    '                         '    | "
+echo -e "\e[34       / .-\  ./=)                                 (=\.  /-. \ "
+echo -e "\e[34      |  |'|_/\/| \e[31mFor I Will Restore Health To You,\e[34 |\/\_|'|  | "
+echo -e "\e[34      ;  |-;| /_|                                   |_\ |;-|  ; "
+echo -e "\e[34     / \_| |/ \   \e[31mand your wounds I will heal,\e[34      | / \| |_/ \ "
+echo -e "\e[34    /      \/\( |                                   | )/\/      \ "
+echo -e "\e[34    |   /  |' ) |                 \e[31mdeclares the \e[1mLORD\e[34 | ( '|  \   | "
+echo -e "\e[34    /   \ _/    |                                   |    \_ /   \ "
+echo -e "\e[34   /--._/  \    |                                   |    /  \_.--\ "
+echo -e "\e[34   `/|)    |    /                                    \   |    (|\` "
+echo -e "\e[34     /     |   |                                    |   |     \ "
+echo -e "\e[34   .'      |   |                                    |   |      '. "
+echo -e "\e[34  /         \  |                                    |  /         \ "
+echo -e "\e[34 (_.-.__.__./  /                                    \  \.__.__.-._) "
